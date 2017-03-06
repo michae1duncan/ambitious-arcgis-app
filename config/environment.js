bea2f463b6e972c63d4fbd1536655801bf9b0868
michae1duncan <michaelgduncan@yahoo.com>
2017-03-06 13:46:50 -0800

    updated torii object

--- config/environment.js
+++ config/environment.js
@@ -24,7 +24,6 @@ module.exports = function(environment) {
         Date: false
       }
     },
-
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
